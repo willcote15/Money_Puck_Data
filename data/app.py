5c206e9e75e8ca9235bf846a9d304709ab4a2281
willcote15 <wcotesmc@gmail.com>
2025-02-23 12:38:12 -0500

    202502231238-updt

--- data/app.py
+++ data/app.py
@@ -2,10 +2,10 @@ import os
 import pandas as pd
 import streamlit as st
 
-# Get the correct absolute path (remove extra "data")
+# Get the absolute path
 csv_path = os.path.join(os.getcwd(), "data", "24-25_skaters_data.csv")
 
-# Debugging: Print the actual path to check
+# Debugging: Print the path to check correctness
 st.write(f"Looking for file at: {csv_path}")
 
 # Check if file exists before loading
